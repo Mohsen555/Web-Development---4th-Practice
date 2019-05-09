8726d16344dd57c841cb79951ce048c69ebd0c2c
Mohsen Kheirabadi <mohsen.kheirabadi@gmail.com>
2019-05-09 18:40:26 -0400

    First Commit

---
+++ app.js
@@ -0,0 +1,71 @@
+// jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req,res){
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/failure", function(req,res){
+  res.redirect("/");
+});
+
+app.post("/", function(req,res){
+
+  var firstName = req.body.fName;
+  var lastName = req.body.lName;
+  var email = req.body.email;
+
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields:{
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  var jsonData = JSON.stringify(data);
+
+  var options = {
+    url: "https://us20.api.mailchimp.com/3.0/lists/28e4e5f614",
+    method: "POST",
+    headers:{
+      "Authorization":"mohsen1 c52689621998579deb3dfa5e031d9252-us20"
+    },
+    body: jsonData
+  };
+
+  request(options, function(error, response, body){
+    if(error){
+      res.sendFile(__dirname + "/failure.html");
+    } else{
+      if(response.statusCode === 200){
+        res.sendFile(__dirname + "/success.html");
+      } else {
+        res.sendFile(__dirname + "/failure.html");
+      }
+    }
+  });
+
+});
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Server is running on port 3000.");
+});
+
+// API Key
+// c52689621998579deb3dfa5e031d9252-us20
+
+// List id (Mailchimp)
+// 28e4e5f614
